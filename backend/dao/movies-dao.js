34f7391baa740f00261761df6c3737372c0520af
favour okwara <favourprogrammer@gmail.com>
2022-12-17 17:16:06 +0200

    add mthod to query and return documents from the movies collection

--- backend/dao/movies-dao.js
+++ backend/dao/movies-dao.js
@@ -15,4 +15,32 @@ class MoviesDao {
             console.error(`Unable to connect to movies collection: ${error}`);
         }
     }
+
+    /**
+     * Queries the movies collection and returns movies matching the filter criteria.
+     * @param {Object} param0 Object containing the criteria for returning a movie.
+     */
+    static async getMovies({ filters = null, pages = 0, moviesPerPage = 20 } = {}) {
+        let query, cursor;
+
+        if (filters) {
+            if ('title' in filters) {
+                query = { $text: { $search: filters['title'] } };
+            } else if ('rated' in filters) {
+                query = { 'rated': { $eq: filters['rated'] } };
+            }
+        }
+
+        try {
+            cursor = await movies
+                .find(query).limit(moviesPerPage).skip(moviesPerPage * pages);
+            
+            const moviesList = await cursor.toArray();
+            const totalNumMovies = await movies.countDocuments(query);
+            return { moviesList, totalNumMovies };
+        } catch (error) {
+            console.error(`Unable to execute find command: ${error}`);
+            return { moviesList: [], totalNumMovies: 0 };
+        }
+    }
 }
