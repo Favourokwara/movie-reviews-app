5f84b9882cd0abbe2ec84652e8df8fcfc384738f
favour okwara <favourprogrammer@gmail.com>
2022-12-17 16:55:54 +0200

    add function to retrieve the movies collection object.

--- backend/dao/movies-dao.js
+++ backend/dao/movies-dao.js
@@ -3,40 +3,16 @@ import { ObjectId } from 'mongodb';
 let movies;
 
 class MoviesDao {
-    /**Retrieves and stores the `movies` collection object within the database.*/
-    static async injectDatabase(connection) {
-        if (movies) { return; }
-
+    /**
+     * Creates an instance of the movies collection object for quering movies.
+     * @param {Object} client MongoClient object for connecting to database.
+     */
+    static async injectDatabase(client) {
+        if (movies) return;
         try {
-            movies = await connection.db(process.env.DB_NAME).collection('movies');
+            movies = await client.db(process.env.DB_NAME).collection('movies')
         } catch (error) {
-            console.error(`unable to connect to ${process.env.DB_NAME}: ${error}`);
-        }
-    }
-
-    static async getMovies({filters = null, pages = 0, moviesPerPage = 20} = {}) {
-        let query, cursor;  // create query document & cursor object with results.
-        
-        if (filters) {
-            if ('title' in filters) {
-                query = {$text: {$search: filters['title']}};
-            }
-            else if ('rated' in filters) {
-                query = {'rated': {$eq: filters['rated']}};
-            }
-        }
-
-        try {
-            cursor = await movies  // query the movies collection for results
-            .find(query).limit(moviesPerPage).skip(moviesPerPage * pages);
-
-            return {
-                moviesList: await cursor.toArray(),
-                totalNumMovies: await movies.countDocuments(),
-            };
-        } catch (error) {
-            console.error(`Unable to query movies: ${error}`);
-            return {moviesList: [], totalNumMovies: 0};
+            console.error(`Unable to connect to movies collection: ${error}`);
         }
     }
 }
