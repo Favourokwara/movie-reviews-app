8d5337568a27a1f96b7927fa896f4db9e220f1d7
favour okwara <favourprogrammer@gmail.com>
2022-12-18 10:58:04 +0200

    add middleware that returns all movies

--- backend/api/controllers/movies-controller.js
+++ backend/api/controllers/movies-controller.js
@@ -1,7 +1,30 @@
 import MoviesDao from '../../dao/movies-dao.js';
 
 class MoviesController {
-    
+    static async apiGetMovies(request, response) {
+        const moviesPerPage = request.query.moviesPerPage
+            ? parseInt(request.query.moviesPerPage) : 20;
+        const page = request.query.page
+            ? parseInt(request.query.page) : 0;
+        
+        let filters = {};
+
+        if (request.query.rated) {
+            filters.rated = request.query.rated;
+
+        } else if (request.query.title) {
+            filters.title = request.query.title;
+        }
+
+        const { moviesList, totalNumMovies } = await MoviesDao
+            .getMovies({ filters, page, moviesPerPage });
+        
+        response.json({
+            movies: moviesList, page, filters,
+            entries_per_page: moviesPerPage,
+            total_results: totalNumMovies
+        })
+    }
 }
 
 export default MoviesController;
