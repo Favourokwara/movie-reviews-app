a572d8ca1fb51a43e4826cd06dcad667d4e2d48d
favour okwara <favourprogrammer@gmail.com>
2022-12-19 10:36:33 +0200

    add method used update reviews

--- backend/api/controllers/reviews-controller.js
+++ backend/api/controllers/reviews-controller.js
@@ -1,7 +1,47 @@
 import ReviewsDao from '../../dao/reviews-dao.js';
 
 class ReviewsController {
-    
+    /**
+     * Adds new review document to the database and responds with an object
+     * with the success status of the executed `POST` request.
+     */
+    static async apiPostReview(request, response) {
+        try {
+            const { movie_id, user_id, name, review } = request.body;
+            const response = await ReviewsDao.addReview(movie_id,
+                { name, _id: user_id }, review, new Date());
+
+            response.json({ status: 'success' });
+
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+
+    /**
+     * Updates the specified review document and responds with an object
+     * with the success status of the executed `PUT` request.
+     */
+    static async apiUpdateReview(request, response) {
+        try {
+            const reviewId = request.body.review_id;
+            const review = request.body.review, date = new Date();
+
+            const ReviewResponse = await ReviewsDao.updateReview(
+                reviewId, request.body.user_id, review, date);
+            const { error } = ReviewResponse;
+
+            if (error) response.status.json({ error });
+            
+            if (ReviewResponse.modifiedCount === 0) {
+                throw new Error('unable to update review. User may not be original poster')
+            }
+            response.json({ status: 'success' });
+
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
 }
 
 export default ReviewsController;
