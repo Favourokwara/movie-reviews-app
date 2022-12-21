3f1567dc171567d9362140cb6b5fb5ac7aabbe0a
favour okwara <favourprogrammer@gmail.com>
2022-12-21 10:39:20 +0200

    create initial state variables for movies list

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function MoviesList() {
+import MovieDataService from '../services/movies';
+
+function MoviesList(props) {
+    const [movies, setMovies] = useState([]);
+    const [searchTitle, setSearchTitle] = useState('');
+    const [searchRating, setSearchRating] = useState('');
+    const [ratings, setRatings] = useState(['All Ratings']);
     return ( 
         <div className="App">
             Movies List
