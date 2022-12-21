e14176d771ad4b18377ddc4762b3b77586b56001
favour okwara <favourprogrammer@gmail.com>
2022-12-21 11:08:33 +0200

    add function to initialize movies & ratings before page is rendered

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -8,6 +8,32 @@ function MoviesList(props) {
     const [searchTitle, setSearchTitle] = useState('');
     const [searchRating, setSearchRating] = useState('');
     const [ratings, setRatings] = useState(['All Ratings']);
+
+    useEffect(
+        () => {
+            retrieveMovies();
+            retrieveRatings();
+        }
+    )
+
+    const retrieveMovies = () => {
+        MovieDataService.getAll()
+            .then(response => {
+                console.log(response.data);
+                setMovies(response.data.movies);
+            })
+            .catch(console.log);
+    }
+
+    const retrieveRatings = () => {
+        MovieDataService.getRatings()
+            .then(response => {
+                console.log(response.data);
+                setRatings(['All Ratings'].concat(response.data));
+            })
+            .catch(console.log)
+    }
+
     return ( 
         <div className="App">
             Movies List
