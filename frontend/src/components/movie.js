f952371a8b704c6e91cdb5a5c6d499510994e5e8
favour okwara <favourprogrammer@gmail.com>
2022-12-23 08:45:05 +0200

    add function to initialise the movie instance

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MovieDataService from '../services/movies';
+import { Link } from 'react-router-dom';
 
-function Movie() {
+function Movie(props) {
+    const [movie, setMovie] = useState({ id: null, title: '', rated: '', reviews: [] });
+
+    const getMovie = id => {
+        MovieDataService.get(id)
+            .then(response => {
+                setMovie(response.data);
+                console.log(response.data);
+            })
+            .catch(console.log);
+    }
+
+    useEffect(() => { getMovie(props.match.params.id) }, [props.match.params.id])
     return (
         <div className="App">
             Movie
